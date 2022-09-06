58e196ee11462b89f9d1bcca8ddd8ecd27df4019
Ogunlade Stephen O <73673531+honordevop@users.noreply.github.com>
2022-09-06 15:25:00 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const list = require('./100-data').list;
+const mapped = list.map(function (value, index) { return value * index; });
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(mapped);
