43d0fbdc93794b244c8f7f03c9c906060df19d8e
Ogunlade Stephen O <73673531+honordevop@users.noreply.github.com>
2022-09-07 15:33:39 +0100

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-"""
-This module implements a base class for Geometry objects,
-with attribute validator functions
-
-Raises:
-    Exception: area is not implemented
-    TypeError: [attribute] must be an integer
-    ValueError: [attribute] ,must be greater than 0
-"""
+# 7-base_geometry.py
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
